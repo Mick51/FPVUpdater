0fd8f8162868a803bd142c54a05ed112c69723b4
Mikael <naudclick.informatik@gmail.com>
2026-06-29 14:24:35 +0200

    Bump version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.example.fpvupdater"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
