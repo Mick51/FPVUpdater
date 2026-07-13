17f5d0d8fa822dc4e2a4a07a908ceef9f7276a85
Mikael <naudclick.informatik@gmail.com>
2026-07-13 14:52:15 +0200

    Bump version to 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.example.fpvupdater"
         minSdk = 24
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
